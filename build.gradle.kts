cf9d6a87cef3cd56dd5dbffb3a65254a28406af5
rawanlazydan <ralzaydan@sdaia.gov.sa>
2022-10-23 01:12:38 +0300

    fix gradel file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,12 @@
-val ktor_version: String by project
-val kotlin_version: String by project
-val logback_version: String by project
+val ktorVersion: String by project
+val kotlinVersion: String by project
+val logbackVersion: String by project
+val koinVersion: String by project
 
 plugins {
     application
     kotlin("jvm") version "1.7.10"
-    id("io.ktor.plugin") version "2.1.0"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.7.10"
 }
 
 group = "com.fitnessAPI"
@@ -17,8 +18,8 @@ application {
 //    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
 //to deploy server
-tasks.create("stage"){
-  dependsOn("installDist")
+tasks.create("stage") {
+    dependsOn("installDist")
 }
 
 repositories {
@@ -26,18 +27,33 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
-    implementation("io.ktor:ktor-serialization-gson-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-    testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+
+    implementation("io.ktor:ktor-server-core:2.1.2")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.1.2")
+    implementation("io.ktor:ktor-server-netty:2.1.2")
+    implementation("ch.qos.logback:logback-classic:1.4.4")
+    implementation("io.ktor:ktor-server-content-negotiation:2.1.2")
+
+    implementation("io.ktor:ktor-server-call-logging:2.1.2")
+    implementation("io.ktor:ktor-server-status-pages:2.1.2")
+    implementation("io.ktor:ktor-server-default-headers:2.1.2")
+
+    testImplementation("io.ktor:ktor-server-tests:2.1.2")
+//    testImplementation("org.jetbrains.kotlin:kotlin-test:$kotlinVersion")
+
+    implementation("io.insert-koin:koin-ktor:3.2.2")
+    implementation("io.insert-koin:koin-core:3.2.2")
+    implementation("io.insert-koin:koin-logger-slf4j:3.2.2")
+
+
+
+
+//    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
 
     implementation("org.litote.kmongo:kmongo:4.7.1")
     implementation("org.litote.kmongo:kmongo-coroutine:4.7.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    implementation("io.ktor:ktor-client-core:$ktor_version")
+    implementation("io.ktor:ktor-client-core:2.1.2")
     implementation("it.skrape:skrapeit:1.2.2")
 
 }
